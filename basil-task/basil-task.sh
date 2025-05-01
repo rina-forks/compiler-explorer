46553c154cce81484419c1701e65ebf851324438
rina <k@rina.fyi>
2025-05-01 16:37:35 +1000

    also print log if output is empty

--- basil-task/basil-task.sh
+++ basil-task/basil-task.sh
@@ -2,19 +2,30 @@
 
 export BIN="${@: -1}"
 export NO_COLOR=1
-export OUT=$(mktemp -p .)
-export LOG=$(mktemp -p .)
+export OUT=$(mktemp -p . --suffix=-out)
+export LOG=$(mktemp -p . --suffix=-log)
+
+# TODO: to defeat the randomly-generated temp directories
+# and enable caching of intermediate objects, cd each task
+# into a directory based on the hash of the binary
+# (or all files in the directory?)
+# (how would this interact with spec files?)
+
+# export TASK_TEMP_DIR=/tmp/task
 
 [[ -n "$OUT" ]] && [[ -n "$BIN" ]] && [[ -n "$LOG" ]]
 
 task_cmd=(task --taskfile "$(dirname "$0")/Taskfile.yml" --dir "$(pwd)" "$@")
 
-if "${task_cmd[@]}" > $LOG 2>&1; then
+if "${task_cmd[@]}" > $LOG 2>&1 && [[ -s "$OUT" ]]; then
   exec cat "$OUT"
 else
-  echo 'error executing basil-task command:'
-  printf '  '
-  printf '%q ' "${task_cmd[@]}" 
+  echo 'error executing command:'
+  echo
+  printf '  '; printf '%q ' "${task_cmd[@]}"; echo
+  # echo "in directory $(pwd)"
+  echo
+  echo 'error output:'
   echo
   exec cat "$LOG"
 fi
